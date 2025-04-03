eb9f49911e32a7efb901678c3db30ec76035558c
doris <doris.condori@gmail.com>
2025-04-02 21:47:56 -0500

    feat: initialize project structure with necessary folders and files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ProductList from '../components/ProductList.vue';
+import ShoppingCart from '../components/ShoppingCart.vue';
+
+</script>
+
+<template>
+    <ProductList></ProductList>
+    <ShoppingCart></ShoppingCart>
+</template>
