dc350970f6a7883d217516bbc2d3c3bc5ac680e2
doris <doris.condori@gmail.com>
2025-04-06 16:39:42 -0500

    feat: complete mobile version of ProductList component

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -15,14 +15,17 @@ defineProps<{
 </script>
 <template>
  <div class="">
-    <div>
-        <img :src="product.image.mobile" :alt="product.name">
+    <div class="">
+        <img :src="product.image.mobile" :alt="product.name" class="rounded-lg">
+        
     </div>
-        <button class="flex bg-white font-redhat"><img src="/images/icon-add-to-cart.svg" alt="cart"> Add to Cart</button>
-    <div>
-        <span>{{ product.category }}</span>
-        <p>{{ product.name }}</p>
-        <p>{{ product.price }}</p>
+    <div class="relative">
+      <button class="flex bg-white font-redhat font-bold text-sm  border p-3 border-gray-400 gap-1 rounded-full px-5 absolute -top-6 left-1/2 -translate-x-1/2"><img src="/images/icon-add-to-cart.svg" alt="cart"> Add to Cart</button>
+    </div>
+    <div class="border py-7">
+        <span class="font-redhat text-stone-500">{{ product.category }}</span>
+        <p class="font-redhatVar font-semibold">{{ product.name }}</p>
+        <p class="font-redhatVar font-semibold text-[#C73B0F]">${{ product.price.toFixed(2) }}</p>
     </div>
  </div>
 </template>
